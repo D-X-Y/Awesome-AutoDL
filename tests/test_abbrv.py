72c59dab1b604b292b9c15689feaaeedbd863977
D-X-Y <280835372@qq.com>
2022-01-18 23:55:07 -0800

    Update data up-to Ref100

--- tests/test_abbrv.py
+++ tests/test_abbrv.py
@@ -15,5 +15,5 @@ class TestAbbrv:
         xfile = str(get_bib_abbrv_file())
         assert os.path.isfile(xfile)
         obj = BibAbbreviations(xfile)
-        assert len(obj) == 37
+        assert len(obj) == 43
         print(obj)
