c2dfd22af6b79ac67eaea848d612d3257fd2ef49
D-X-Y <280835372@qq.com>
2022-01-21 00:11:53 -0800

    Sync initial paper refs done

--- tests/test_abbrv.py
+++ tests/test_abbrv.py
@@ -15,5 +15,5 @@ class TestAbbrv:
         xfile = str(get_bib_abbrv_file())
         assert os.path.isfile(xfile)
         obj = BibAbbreviations(xfile)
-        assert len(obj) == 57
+        assert len(obj) == 58
         print(obj)
